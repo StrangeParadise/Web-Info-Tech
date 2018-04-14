0b248cfaec40afd6dd3310e73f0391fff10f8a46
BrainOverFlow <zoranlang915@gmail.com>
2018-04-14 10:03:31 +1000

    bug fix

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,6 @@
-module.exports.renderComingSoon = function (req, res) {
+module.exports.renderIndex = function (req, res) {
     res.render('index');
+}
+module.exports.renderComingSoon = function (req, res) {
+    res.render('comingSoon');
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controller');
 
+router.get('/', controller.renderIndex);
 router.get('/comingSoon', controller.renderComingSoon);
 
 module.exports = router;
